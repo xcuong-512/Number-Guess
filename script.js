c021e121d9b74437a581c53394ac38eb65f8ffb5
xcuong-512 <xcuong286@gmail.com>
2025-11-06 08:32:34 +0700

    Init Project

---
+++ script.js
@@ -0,0 +1,22 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+// Test Random Number Function
+// console.log(generateTarget());
+
+const compareGuesses = (humanGuess, comGuess, targetNumber) => {
+    if (humanGuess === targetNumber) return `Human Win!`;
+    else if (comGuess === targetNumber) return 'Computer Win!';
+    else 'Human Win';
+}
+
+const updateScore = (output) => {
+    if (output === 'human') humanScore++;
+    else if (output === 'computer') computerScore++;
+}
